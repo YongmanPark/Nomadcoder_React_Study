55a0db31bdc14990737d7e97475370bb51400f42
yongman park <yongman514@gmail.com>
2023-06-09 12:39:24 +0900

    todolist with react

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,24 +1,31 @@
 import Button from "./Button";
 import { useState, useEffect } from "react";
 
-function Hello(){
-  useEffect(()=>{
-    console.log("effec사용")
-  }, []);
-  return <h1>HI</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {
-    setShowing((prev) => !prev);
-  }
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return ;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo(""); 
+  };
   return (
     <div>
+      <h1>Todo Practice {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="todo"/>
+        <button>Click</button>
+      </form>
+      <hr/>
       {
-        showing ? <Hello/> : null
+        toDos.map((item , i) => <li key={i}>{item}</li>)
       }
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
