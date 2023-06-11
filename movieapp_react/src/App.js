96c6b81e31da0251bf09ac1b3a8e992bc09f3895
yongman park <yongman514@gmail.com>
2023-06-11 21:38:15 +0900

    react movie app

--- movieapp_react/src/App.js
+++ movieapp_react/src/App.js
@@ -4,19 +4,30 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  const getMovies = async() =>{
-    const json = await (
+  const getMovies = async () => {
+    const json  = await(
       await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json(); 
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  console.log(movies);
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )  
+    ).json();
+      setMovies(json.data.movies);
+      setLoading(false);
+  };
+  useEffect(()=>{
+    getMovies();
+  }, []);
+
   return (
     <div className="App">
-      {loading ? <h3>loading...</h3> : null}
+      {loading ? <h3>loading...</h3> : 
+        <div>{movies.map( (movies) => <div key={movies.id}>
+          <img src={movies.medium_cover_image}></img>
+          {movies.title} <p>{movies.summary}</p>
+        <ul>
+          {movies.genres.map((g) => <li key={g}>{g}</li>)}
+        </ul>
+        </div>)}</div>
+      }
     </div>
   );
 }
